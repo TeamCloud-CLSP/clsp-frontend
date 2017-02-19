b9a8b2bf7570622a8bf0e38179058740c6e3812a
kevintasta <yuanhan_pan@yahoo.com>
2017-02-19 15:07:32 -0500

    Added embed videos to course detail page

--- src/app/components/course-detail.component.js
+++ src/app/components/course-detail.component.js
@@ -11,20 +11,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var common_1 = require('@angular/common');
+var platform_browser_1 = require('@angular/platform-browser');
 var course_service_1 = require('../services/course.service');
 var course_1 = require('../models/course');
 require('rxjs/add/operator/switchMap');
 var CourseDetailComponent = (function () {
-    function CourseDetailComponent(courseService, route, location) {
+    function CourseDetailComponent(courseService, route, location, sanitizer) {
         this.courseService = courseService;
         this.route = route;
         this.location = location;
+        this.sanitizer = sanitizer;
     }
     CourseDetailComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.route.params
             .switchMap(function (params) { return _this.courseService.getCourse(+params['id']); })
             .subscribe(function (course) { return _this.course = course; });
+        this.edited = false;
     };
     CourseDetailComponent.prototype.goBack = function () {
         this.location.back();
@@ -33,6 +36,13 @@ var CourseDetailComponent = (function () {
         var _this = this;
         this.courseService.update(this.course)
             .then(function () { return _this.goBack(); });
+    };
+    CourseDetailComponent.prototype.addMedia = function () {
+        console.log(this.url);
+        this.edited = true;
+    };
+    CourseDetailComponent.prototype.getSafeURL = function () {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(this.media);
     };
     __decorate([
         core_1.Input(), 
@@ -44,7 +54,7 @@ var CourseDetailComponent = (function () {
             selector: 'course-detail',
             templateUrl: '../templates/course-detail.component.html'
         }), 
-        __metadata('design:paramtypes', [course_service_1.CourseService, router_1.ActivatedRoute, common_1.Location])
+        __metadata('design:paramtypes', [course_service_1.CourseService, router_1.ActivatedRoute, common_1.Location, platform_browser_1.DomSanitizer])
     ], CourseDetailComponent);
     return CourseDetailComponent;
 }());
--- src/app/components/course-detail.component.ts
+++ src/app/components/course-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import { CourseService } from '../services/course.service';
 import { Course } from '../models/course';
 import 'rxjs/add/operator/switchMap';
@@ -13,17 +14,22 @@ import 'rxjs/add/operator/switchMap';
 
 export class CourseDetailComponent implements OnInit {
     @Input() course: Course;
+    media: string;
+    edited: boolean;
+    url: SafeResourceUrl;
 
     constructor(
         private courseService: CourseService,
         private route: ActivatedRoute,
-        private location: Location
+        private location: Location,
+        private sanitizer: DomSanitizer
     ) { }
 
     ngOnInit(): void {
         this.route.params
             .switchMap((params: Params) => this.courseService.getCourse(+params['id']))
             .subscribe(course => this.course = course);
+        this.edited = false;
     }
 
     goBack(): void {
@@ -33,5 +39,14 @@ export class CourseDetailComponent implements OnInit {
     save(): void {
         this.courseService.update(this.course)
             .then(() => this.goBack());
+    }
+
+    addMedia(): void {
+        console.log(this.url)
+        this.edited = true;
+    }
+
+    getSafeURL() {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(this.media);
     }
 }
