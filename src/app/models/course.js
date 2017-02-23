9e8f949f3b70708b388e3e6cc96a848789e01aea
Patrick Tam <pjztam@gmail.com>
2017-02-22 23:21:10 -0500

    removed untracked files

--- src/app/models/course.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Course = (function () {
-    function Course() {
-    }
-    return Course;
-}());
-exports.Course = Course;
-//# sourceMappingURL=course.js.map
--- src/app/models/profregistration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ProfRegistration = (function () {
-    function ProfRegistration() {
-    }
-    return ProfRegistration;
-}());
-exports.ProfRegistration = ProfRegistration;
-//# sourceMappingURL=profregistration.js.map
