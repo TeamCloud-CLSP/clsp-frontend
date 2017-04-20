c3afa36690bf265f69ecaba611011776ddc4879a
kevintasta <yuanhan_pan@yahoo.com>
2017-04-20 18:15:00 -0400

    Next and previous topic buttons for student question view

--- src/app/components/student/student-header-detail.component.ts
+++ src/app/components/student/student-header-detail.component.ts
@@ -9,186 +9,195 @@ import { StudentService } from '../../services/student.service';
 import 'rxjs/add/operator/switchMap';
 
 @Pipe({
-    name: 'getChoices',
-    pure: false
+  name: 'getChoices',
+  pure: false
 })
 export class GetChoicesPipe implements PipeTransform {
-    transform(choiceString: string, object: any): Choice[] {
-        return JSON.parse(choiceString) as Choice[];
-    }
+  transform(choiceString: string, object: any): Choice[] {
+    return JSON.parse(choiceString) as Choice[];
+  }
 }
 
 @Component({
-    selector: 'app-module-detail',
-    templateUrl: '../../templates/student/student-header-detail.component.html'
+  selector: 'app-module-detail',
+  templateUrl: '../../templates/student/student-header-detail.component.html'
 })
 export class StudentHeaderDetailComponent implements OnInit {
-    unit: Unit;
-    courseName: string;
-    song: Song;
-    header: Header;
-    questions: Question[];
-    newQuestion: Question;
-    selectedType: string;
-    choices: Choice[];
-    mcAnswer: string;
-    msAnswers: string[];
-    fbAnswers: Choice[];
-    printVersion: boolean;
-    answerChecks: AnswerCheck[];
+  unit: Unit;
+  courseName: string;
+  song: Song;
+  header: Header;
+  questions: Question[];
+  newQuestion: Question;
+  selectedType: string;
+  choices: Choice[];
+  mcAnswer: string;
+  msAnswers: string[];
+  fbAnswers: Choice[];
+  printVersion: boolean;
+  answerChecks: AnswerCheck[];
+  prevId: number;
+  nextId: number;
 
-    constructor(
-        private route: ActivatedRoute,
-        private location: Location,
-        private sanitizer: DomSanitizer,
-        private router: Router,
-        private studentService: StudentService
-    ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    private studentService: StudentService
+  ) { }
 
-    ngOnInit(): void {
-        this.route.params
-            .switchMap((params: Params) => this.studentService.getUnit(+params['unit_id']))
-            .subscribe(unit => {
-                this.unit = unit;
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.studentService.getUnit(+params['unit_id']))
+      .subscribe(unit => {
+        this.unit = unit;
 
-                this.studentService.getCourse(unit.course_id).then(course => {
-                    this.courseName = course;
-                });
-            });
-        this.route.params
-            .switchMap((params: Params) => this.studentService.getSong(+params['song_id']))
-            .subscribe(song => {
-                this.song = song;
-            });
+        this.studentService.getCourse(unit.course_id).then(course => {
+          this.courseName = course;
+        });
+      });
+    this.route.params
+      .switchMap((params: Params) => this.studentService.getSong(+params['song_id']))
+      .subscribe(song => {
+        this.song = song;
+      });
 
-        this.route.params
-            .switchMap((params: Params) => this.studentService.getHeader(+params['header_id']))
-            .subscribe(header => {
-                this.header = header;
-            });
+    this.route.params
+      .switchMap((params: Params) => this.studentService.getHeader(+params['header_id']))
+      .subscribe(header => {
+        this.header = header;
+      });
 
-        this.route.params
-            .switchMap((params: Params) => this.studentService.getQuestions(+params['header_id']))
-            .subscribe(questions => {
-                this.questions = questions.sort((x, y) => x.weight - y.weight);
-                console.log(questions);
-                for (let question of this.questions) {
-                    console.log(question.choices);
-                    if(question.choices) question.displayChoices = JSON.parse(question.choices) as Choice[];
-                    console.log(question.displayChoices);
-                    if (question.type == "fill-blank") {
-                        var blankNumber = (question.content.split("_").length - 1)
-                        question.fbPieces = this.splitBlanks(question.content, blankNumber);
-                        question.fbAnswers = [];
-                        for (var i = 0; i < blankNumber; i++) {
-                            var choice = new Choice();
-                            choice.choice = "";
-                            question.fbAnswers.push(choice);
-                        }
-                    }
-                }
-            });
-    }
-
-    getChoices(choiceString: string): Choice[] {
-        return JSON.parse(choiceString) as Choice[];
-    }
-
-    convertAscii(num: number): String {
-        return String.fromCharCode(97 + num).toUpperCase();
-    }
-
-    printerVersion(): void {
-        this.printVersion = true;
-    }
-
-    cancelPrint(): void {
-        this.printVersion = false;
-    }
-
-    splitBlanks(question: string, blanks: number): string[] {
-        for (var i = 0; i < blanks; i++) {
-            question = question.replace('_', "|" + i.toString() + "|");
+    this.route.params
+      .switchMap((params: Params) => this.studentService.getQuestions(+params['header_id']))
+      .subscribe(questions => {
+        this.questions = questions.questions;
+        this.prevId = questions.prev;
+        this.nextId = questions.next;
+        this.questions = this.questions.sort((x, y) => x.weight - y.weight);
+        console.log(questions);
+        for (let question of this.questions) {
+          console.log(question.choices);
+          if (question.choices) question.displayChoices = JSON.parse(question.choices) as Choice[];
+          console.log(question.displayChoices);
+          if (question.type == "fill-blank") {
+            var blankNumber = (question.content.split("_").length - 1)
+            question.fbPieces = this.splitBlanks(question.content, blankNumber);
+            question.fbAnswers = [];
+            for (var i = 0; i < blankNumber; i++) {
+              var choice = new Choice();
+              choice.choice = "";
+              question.fbAnswers.push(choice);
+            }
+          }
         }
-        var pieces = question.split('|').filter(x => x != "");
-        console.log(pieces);
-        return pieces;
+      });
+  }
+
+  getChoices(choiceString: string): Choice[] {
+    return JSON.parse(choiceString) as Choice[];
+  }
+
+  convertAscii(num: number): String {
+    return String.fromCharCode(97 + num).toUpperCase();
+  }
+
+  printerVersion(): void {
+    this.printVersion = true;
+  }
+
+  cancelPrint(): void {
+    this.printVersion = false;
+  }
+
+  splitBlanks(question: string, blanks: number): string[] {
+    for (var i = 0; i < blanks; i++) {
+      question = question.replace('_', "|" + i.toString() + "|");
     }
+    var pieces = question.split('|').filter(x => x != "");
+    console.log(pieces);
+    return pieces;
+  }
 
-    checkBlank(piece: string): boolean {
-        return isNaN(parseInt(piece));
-    }
+  checkBlank(piece: string): boolean {
+    return isNaN(parseInt(piece));
+  }
 
 
-    checkAnswers() {
-        var test = "{";
-        var check = this.questions.filter(x => x.type == "fill-blank" || x.type == "multiple-choice" || x.type == "multiple-select");
-        for (let q of check) {
-            if (q.type == "fill-blank") {
-                if (q.fbAnswers == null || q.fbAnswers.length < 1) {
-                    let choices: Choice[] = [];
-                    var choice = new Choice();
-                    choice.choice = "";
-                    choices.push(choice);
-                    var answers = JSON.stringify(choices);
-                } else {
-                    var answers = JSON.stringify(q.fbAnswers);
-                }
-                test = test + "\"" + q.id + "\" : " + answers + ",";
-            }
-            else if (q.type == "multiple-choice") {
-                var choice = new Choice();
-                if (q.mcAnswer == null) {
-                    choice.choice = "";
-                } else {
-                    choice.choice = q.mcAnswer;
-                }
-                var answers = JSON.stringify([choice]);
-                test = test + "\"" + q.id + "\" : " + answers + ",";
-            }
-            else if (q.type == "multiple-select") {
-                let choices: Choice[] = [];
-                if (q.msAnswers == null || q.msAnswers.length < 1) {
-                    let choices: Choice[] = [];
-                    var choice = new Choice();
-                    choice.choice = "";
-                    choices.push(choice);
-                    var answers = JSON.stringify(choices);
-                } else {
-                    for (let a of q.msAnswers) {
-                        var choice = new Choice();
-                        choice.choice = a;
-                        choices.push(choice);
-                    }
-                    var answers = JSON.stringify(choices);
-                }
-                test = test + "\"" + q.id + "\" : " + answers + ",";
-            }
+  checkAnswers() {
+    var test = "{";
+    var check = this.questions.filter(x => x.type == "fill-blank" || x.type == "multiple-choice" || x.type == "multiple-select");
+    for (let q of check) {
+      if (q.type == "fill-blank") {
+        if (q.fbAnswers == null || q.fbAnswers.length < 1) {
+          let choices: Choice[] = [];
+          var choice = new Choice();
+          choice.choice = "";
+          choices.push(choice);
+          var answers = JSON.stringify(choices);
+        } else {
+          var answers = JSON.stringify(q.fbAnswers);
         }
-        test = test.substring(0, test.length - 1);
-        test = test + "}";
-        this.studentService.checkAnswers(test)
-            .then(
-            response => {
-                //console.log(response);
-                for (let a of response) {
-                    var choice = this.questions.find(x => x.id == a.id);
-                    choice.correct = a.result;
-                }
-                console.log(this.questions);
-            }
-            )
-    }
-    isMsAnswer(question: Question, answer: string) {
-        if (question.msAnswers == null || question.msAnswers.length < 1) {
-            return false;
+        test = test + "\"" + q.id + "\" : " + answers + ",";
+      }
+      else if (q.type == "multiple-choice") {
+        var choice = new Choice();
+        if (q.mcAnswer == null) {
+          choice.choice = "";
+        } else {
+          choice.choice = q.mcAnswer;
         }
-        return question.msAnswers.indexOf(answer) > -1;
+        var answers = JSON.stringify([choice]);
+        test = test + "\"" + q.id + "\" : " + answers + ",";
+      }
+      else if (q.type == "multiple-select") {
+        let choices: Choice[] = [];
+        if (q.msAnswers == null || q.msAnswers.length < 1) {
+          let choices: Choice[] = [];
+          var choice = new Choice();
+          choice.choice = "";
+          choices.push(choice);
+          var answers = JSON.stringify(choices);
+        } else {
+          for (let a of q.msAnswers) {
+            var choice = new Choice();
+            choice.choice = a;
+            choices.push(choice);
+          }
+          var answers = JSON.stringify(choices);
+        }
+        test = test + "\"" + q.id + "\" : " + answers + ",";
+      }
     }
+    test = test.substring(0, test.length - 1);
+    test = test + "}";
+    this.studentService.checkAnswers(test)
+      .then(
+      response => {
+        //console.log(response);
+        for (let a of response) {
+          var choice = this.questions.find(x => x.id == a.id);
+          choice.correct = a.result;
+        }
+        console.log(this.questions);
+      }
+      )
+  }
+  isMsAnswer(question: Question, answer: string) {
+    if (question.msAnswers == null || question.msAnswers.length < 1) {
+      return false;
+    }
+    return question.msAnswers.indexOf(answer) > -1;
+  }
 
-    getFbCheck(correct: string, index: number): string {
-        console.log(correct);
-        return correct.split(',')[index];
-    }
+  getFbCheck(correct: string, index: number): string {
+    console.log(correct);
+    return correct.split(',')[index];
+  }
+
+  toHeader(headerId: number): void {
+    this.router.navigate(['./student/unit/' + this.unit.id + '/song/' + this.song.id + '/header/' + headerId]);
+  }
 
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -3,148 +3,154 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import { StudentClass } from '../models/clsp-class';
 import { Unit, Song, Module, Media } from '../models/course';
-import { Header, Question, AnswerCheck } from '../models/modules/header';
+import { Header, Question, AnswerCheck, Items } from '../models/modules/header';
 import { CulturalNote } from '../models/modules/CulturalNote';
 import { AuthenticationService } from './authentication.service';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class StudentService {
-    private baseUrl = environment.apiBase + 'api/student';
+  private baseUrl = environment.apiBase + 'api/student';
 
-    constructor(private http: Http, private authService: AuthenticationService) {
+  constructor(private http: Http, private authService: AuthenticationService) {
 
-    }
+  }
 
-    private handleError(error: any): Promise<any> {
-        console.error('An error occured', error);
-        return Promise.reject(error.message || error);
-    }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
 
-    getClass(): Promise<StudentClass> {
-        // TODO: response returns nothing, backend problems
-        // var sample = {"id":1,"name":"CHIN 3002 A",
-        //     "description":"advanced chinese course","date_start":0,"date_end":1497057872,
-        //     "course_id":1,"registration_id":1};
-        return this.http.get(this.baseUrl + '/class', this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json() as StudentClass;
-            })
-            .catch(this.handleError);
-    }
+  getClass(): Promise<StudentClass> {
+    // TODO: response returns nothing, backend problems
+    // var sample = {"id":1,"name":"CHIN 3002 A",
+    //     "description":"advanced chinese course","date_start":0,"date_end":1497057872,
+    //     "course_id":1,"registration_id":1};
+    return this.http.get(this.baseUrl + '/class', this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json() as StudentClass;
+      })
+      .catch(this.handleError);
+  }
 
-    getCourse(courseId: number): Promise<string> {
-        console.log(courseId);
-        return this.http.get(this.baseUrl + '/course/' + courseId, this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                const name = response.json().name;
-                return name;
-            })
-            .catch(this.handleError);
-    }
+  getCourse(courseId: number): Promise<string> {
+    console.log(courseId);
+    return this.http.get(this.baseUrl + '/course/' + courseId, this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        const name = response.json().name;
+        return name;
+      })
+      .catch(this.handleError);
+  }
 
-    getUnits(courseId: number): Promise<Unit[]> {
-        return this.http.get(this.baseUrl + '/course/' + courseId + '/units', this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json().data as Unit[];
-            })
-            .catch(this.handleError);
-    }
+  getUnits(courseId: number): Promise<Unit[]> {
+    return this.http.get(this.baseUrl + '/course/' + courseId + '/units', this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json().data as Unit[];
+      })
+      .catch(this.handleError);
+  }
 
-    getUnit(unitId: number): Promise<Unit> {
-        return this.http.get(this.baseUrl + '/unit/' + unitId, this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json() as Unit;
-            })
-            .catch(this.handleError);
-    }
+  getUnit(unitId: number): Promise<Unit> {
+    return this.http.get(this.baseUrl + '/unit/' + unitId, this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json() as Unit;
+      })
+      .catch(this.handleError);
+  }
 
-    getSongs(unitId: number): Promise<Song[]> {
-        return this.http.get(this.baseUrl + '/unit/' + unitId + '/songs', this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json().data as Song[];
-            })
-            .catch(this.handleError);
-    }
+  getSongs(unitId: number): Promise<Song[]> {
+    return this.http.get(this.baseUrl + '/unit/' + unitId + '/songs', this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json().data as Song[];
+      })
+      .catch(this.handleError);
+  }
 
-    getSong(songId: number): Promise<any> {
-        return this.http.get(this.baseUrl + '/song/' + songId, this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json();
-            })
-            .catch(this.handleError);
-    }
+  getSong(songId: number): Promise<any> {
+    return this.http.get(this.baseUrl + '/song/' + songId, this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json();
+      })
+      .catch(this.handleError);
+  }
 
-    getModules(songId: number): Promise<Module[]> {
-        return this.http.get(this.baseUrl + '/song/' + songId + '/modules', this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json().data as Module[];
-            })
-            .catch(this.handleError);
-    }
+  getModules(songId: number): Promise<Module[]> {
+    return this.http.get(this.baseUrl + '/song/' + songId + '/modules', this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json().data as Module[];
+      })
+      .catch(this.handleError);
+  }
 
-    getMedia(songId: number): Promise<Media[]> {
-        return this.http.get(this.baseUrl + '/song/' + songId + '/media', this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                return response.json().data as Media[];
-            })
-            .catch(this.handleError);
-    }
+  getMedia(songId: number): Promise<Media[]> {
+    return this.http.get(this.baseUrl + '/song/' + songId + '/media', this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        return response.json().data as Media[];
+      })
+      .catch(this.handleError);
+  }
 
-    getHeaders(songId: number, modName: string): Promise<Header[]> {
-        const url = `${this.baseUrl}/song/${songId}/${modName}/headers`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as Header[])
-            .catch(this.handleError);
-    }
+  getHeaders(songId: number, modName: string): Promise<Header[]> {
+    const url = `${this.baseUrl}/song/${songId}/${modName}/headers`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json().data as Header[])
+      .catch(this.handleError);
+  }
 
-    getHeader(headerId: number): Promise<Header> {
-        const url = `${this.baseUrl}/header/${headerId}`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json() as Header)
-            .catch(this.handleError);
-    }
+  getHeader(headerId: number): Promise<Header> {
+    const url = `${this.baseUrl}/header/${headerId}`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json() as Header)
+      .catch(this.handleError);
+  }
 
-    getQuestions(headerId: number): Promise<Question[]> {
-        const url = `${this.baseUrl}/header/${headerId}/items`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as Question[])
-            .catch(this.handleError);
-    }
+  getQuestions(headerId: number): Promise<Items> {
+    const url = `${this.baseUrl}/header/${headerId}/items`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        var items = new Items();
+        items.questions = response.json().data as Question[];
+        items.next = response.json().next_id;
+        items.prev = response.json().prev_id;
+        return items;
+      })
+      .catch(this.handleError);
+  }
 
-    getCulturalNote(songId: number): Promise<CulturalNote[]> {
-        const url = `${this.baseUrl}/song/${songId}/keywords`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => {
-                const data = response.json().data as CulturalNote[];
-                for (let i = 0; i < data.length; i++) {
-                    data[i].songId = songId;
-                }
-                return data;
-            })
-            .catch(error => {
-                console.log('ERRORR');
-            });
-    }
+  getCulturalNote(songId: number): Promise<CulturalNote[]> {
+    const url = `${this.baseUrl}/song/${songId}/keywords`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => {
+        const data = response.json().data as CulturalNote[];
+        for (let i = 0; i < data.length; i++) {
+          data[i].songId = songId;
+        }
+        return data;
+      })
+      .catch(error => {
+        console.log('ERRORR');
+      });
+  }
 
-    checkAnswers(question: string): Promise<AnswerCheck[]> {
-        const url = `${this.baseUrl}/checkitems`;
-        return this.http.post(url, question, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as AnswerCheck[])
-            .catch(this.handleError);
-    }
+  checkAnswers(question: string): Promise<AnswerCheck[]> {
+    const url = `${this.baseUrl}/checkitems`;
+    return this.http.post(url, question, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json().data as AnswerCheck[])
+      .catch(this.handleError);
+  }
 
 }
