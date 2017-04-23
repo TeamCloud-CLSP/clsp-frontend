9779b004129a0b3c855abf9e1b52f1236545f7e8
kevintasta <yuanhan_pan@yahoo.com>
2017-04-23 15:11:41 -0400

    Student registration ui updates and added editor for max registrations

--- src/app/models/studentregistration.ts
+++ src/app/models/studentregistration.ts
@@ -1,13 +1,15 @@
 export class StudentRegistration {
-    class_id: number;
-    class_name: string;
-    student_registration_id: number;
-    student_registration_date_start: number;
-    student_registration_date_end: number;
-    constructor() { }
+  class_id: number;
+  class_name: string;
+  student_registration_id: number;
+  student_registration_date_start: number;
+  student_registration_date_end: number;
+  student_registration_max_registrations: number;
+  signup_code: string;
+  constructor() { }
 }
 
 export class Student {
-    id: number;
-    username: string;
+  id: number;
+  username: string;
 }
--- src/app/services/professor.service.ts
+++ src/app/services/professor.service.ts
@@ -9,89 +9,95 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ProfessorService {
-    private baseUrl = environment.apiBase + 'api/professor';
+  private baseUrl = environment.apiBase + 'api/professor';
 
-    constructor(private http: Http,
-                private authService: AuthenticationService) {
+  constructor(private http: Http,
+    private authService: AuthenticationService) {
 
-    }
+  }
 
-    private handleError(error: any): Promise<any> {
-        console.error('An error occured', error);
-        return Promise.reject(error.message || error);
-    }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
 
-    getClasses(): Promise<CLSPClass[]> {
-        return this.http.get(this.baseUrl + '/classes', this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as CLSPClass[])
-            .catch(this.handleError);
-    }
+  getClasses(): Promise<CLSPClass[]> {
+    return this.http.get(this.baseUrl + '/classes', this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json().data as CLSPClass[])
+      .catch(this.handleError);
+  }
 
-    getClass(id: number): Promise<SingleClass> {
-        return this.http.get(this.baseUrl + '/classes/' + id, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json() as SingleClass)
-            .catch(this.handleError);
-    }
+  getClass(id: number): Promise<SingleClass> {
+    return this.http.get(this.baseUrl + '/classes/' + id, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json() as SingleClass)
+      .catch(this.handleError);
+  }
 
-    getDashboard(): Promise<ProfInfo[]> {
-        const url = `${this.baseUrl}/main`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as ProfInfo[])
-            .catch(this.handleError);
-    }
+  getDashboard(): Promise<ProfInfo[]> {
+    const url = `${this.baseUrl}/main`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json().data as ProfInfo[])
+      .catch(this.handleError);
+  }
 
-    createClass(clspClass: CreateClass): Promise<SingleClass> {
-        const url = `${this.baseUrl}/classes`;
-        console.log(clspClass);
-        return this.http.post(url, JSON.stringify({ name: clspClass.name, course_id: clspClass.course_id, description: clspClass.description }), this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json() as SingleClass)
-            .catch(this.handleError);
-    }
+  createClass(clspClass: CreateClass): Promise<SingleClass> {
+    const url = `${this.baseUrl}/classes`;
+    console.log(clspClass);
+    return this.http.post(url, JSON.stringify({ name: clspClass.name, course_id: clspClass.course_id, description: clspClass.description }), this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json() as SingleClass)
+      .catch(this.handleError);
+  }
 
-    deleteClass(id: number): Promise<void> {
-        const url = `${this.baseUrl}/classes/${id}`;
-        return this.http.delete(url, this.authService.getOptions())
-            .toPromise()
-            .then(() => null)
-            .catch(this.handleError);
-    }
+  deleteClass(id: number): Promise<void> {
+    const url = `${this.baseUrl}/classes/${id}`;
+    return this.http.delete(url, this.authService.getOptions())
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
-    getStudentRegistrationForClass(id: number): Promise<StudentRegistration> {
-        const url = `${this.baseUrl}/classes/${id}/registrations/student`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json() as StudentRegistration)
-            .catch(null);
-    }
+  getStudentRegistrationForClass(id: number): Promise<StudentRegistration> {
+    const url = `${this.baseUrl}/classes/${id}/registrations/student`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json() as StudentRegistration)
+      .catch(null);
+  }
 
-    createStudentRegistration(reg: StudentRegistration): Promise<void> {
-        console.log(reg);
-        const url = `${this.baseUrl}/registrations/student`;
-        return this.http.post(url, JSON.stringify({ date_start: reg.student_registration_date_start, date_end: reg.student_registration_date_end, class_id: reg.class_id }), this.authService.getOptions())
-            .toPromise()
-            .then(() => null)
-            .catch(this.handleError);
-    }
+  createStudentRegistration(reg: StudentRegistration): Promise<void> {
+    console.log(reg);
+    const url = `${this.baseUrl}/registrations/student`;
+    return this.http.post(url, JSON.stringify({
+      date_start: reg.student_registration_date_start,
+      date_end: reg.student_registration_date_end,
+      class_id: reg.class_id,
+      max_registrations: reg.student_registration_max_registrations
+    }),
+      this.authService.getOptions())
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
-    getStudents(regId: number): Promise<Student[]> {
-        const url = `${this.baseUrl}/registrations/student/${regId}/students`;
-        return this.http.get(url, this.authService.getOptions())
-            .toPromise()
-            .then(response => response.json().data as Student[])
-            .catch(this.handleError);
-    }
+  getStudents(regId: number): Promise<Student[]> {
+    const url = `${this.baseUrl}/registrations/student/${regId}/students`;
+    return this.http.get(url, this.authService.getOptions())
+      .toPromise()
+      .then(response => response.json().data as Student[])
+      .catch(this.handleError);
+  }
 
-    deleteStudent(studentId: number): Promise<void> {
-        const url = `${this.baseUrl}/student/${studentId}`;
-        return this.http.delete(url, this.authService.getOptions())
-            .toPromise()
-            .then(() => null)
-            .catch(this.handleError);
-    }
+  deleteStudent(studentId: number): Promise<void> {
+    const url = `${this.baseUrl}/student/${studentId}`;
+    return this.http.delete(url, this.authService.getOptions())
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
 
 }
