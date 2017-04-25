1ab3ad94eacefe915ba7fc830bcff31c38214816
kevintasta <yuanhan_pan@yahoo.com>
2017-04-24 22:13:53 -0400

    Readded unit description to student dashboard

--- src/app/components/student-dashboard.component.ts
+++ src/app/components/student-dashboard.component.ts
@@ -3,54 +3,56 @@ import { StudentClass } from '../models/clsp-class';
 import { Router } from '@angular/router';
 import { StudentService } from '../services/student.service';
 import { Unit, Song } from '../models/course';
+import {SafeHtmlPipe} from './safe-html.pipe'
+
 
 @Component({
-    selector: 'app-student-dashboard',
-    templateUrl: '../templates/student-dashboard.component.html'
+  selector: 'app-student-dashboard',
+  templateUrl: '../templates/student-dashboard.component.html'
 })
 
 export class StudentDashboardComponent implements OnInit {
 
-    public myClass: StudentClass;
-    public units: Unit[];
-    public songs: Song[][];
-    public loadedSongs: boolean;
+  public myClass: StudentClass;
+  public units: Unit[];
+  public songs: Song[][];
+  public loadedSongs: boolean;
 
-    constructor(private service: StudentService,
-                private router: Router) {
-    }
+  constructor(private service: StudentService,
+    private router: Router) {
+  }
 
-    ngOnInit(): void {
-        this.songs = [];
-        this.loadedSongs = false;
-        this.service.getClass().then(myClass => {
-            this.myClass = myClass;
-        }).then(() => {
-            this.service.getUnits(this.myClass.course_id).then(units => {
-                this.units = units;
-            }).then(() => {
-                for (let i = 0; i < this.units.length; i++) {
-                    this.service.getSongs(this.units[i].id).then(songs => {
-                        this.units[i].songs = songs;
-                    });
-                }
-                this.loadedSongs = true;
-            });
-        });
-    }
-
-    goToUnit(unit: Unit) {
-        this.router.navigate(['./student/unit', unit.id]);
-    }
-
-    hasSongs(unit_id: number) {
-        return this.songs[unit_id].length > 0;
-    }
-
-    getShortenedDescription(unit: Unit): string {
-        if (unit.description.length > 305) {
-            return unit.description.substr(0, 300) + "...";
+  ngOnInit(): void {
+    this.songs = [];
+    this.loadedSongs = false;
+    this.service.getClass().then(myClass => {
+      this.myClass = myClass;
+    }).then(() => {
+      this.service.getUnits(this.myClass.course_id).then(units => {
+        this.units = units;
+      }).then(() => {
+        for (let i = 0; i < this.units.length; i++) {
+          this.service.getSongs(this.units[i].id).then(songs => {
+            this.units[i].songs = songs;
+          });
         }
-        return unit.description;
+        this.loadedSongs = true;
+      });
+    });
+  }
+
+  goToUnit(unit: Unit) {
+    this.router.navigate(['./student/unit', unit.id]);
+  }
+
+  hasSongs(unit_id: number) {
+    return this.songs[unit_id].length > 0;
+  }
+
+  getShortenedDescription(unit: Unit): string {
+    if (unit.description.length > 305) {
+      return unit.description.substr(0, 300) + "...";
     }
+    return unit.description;
+  }
 }
